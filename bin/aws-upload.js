9965ff59f0056ea3cc9c4546aa23b1437c446249
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-03-06 15:08:39 -0500

    Refactors bin script into module
    Moved most of the code from bin/aws-upload that was responsible for
    the s3 upload piece to it's own file in the lib directory. I saw the
    need for using this code in more than one place, so the lib/se-upload
    module can now be used from more than one place in our application.
    The bin/aws-upload script still works (and just implements the lib one)

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -1,63 +1,13 @@
 'use strict';
 
 require('dotenv').load();
-
-const mime = require('mime');
-const path = require('path');
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-const fs = require('fs');
-const crypto = require('crypto');
+const s3Upload =  require('../lib/s3-upload');
 
 let file = {
   path: process.argv[2],
   title: process.argv[3]
 };
 
-let mimeType = mime.lookup(file.path);
-let ext = path.extname(file.path);
-let folder = (new Date()).toISOString().split('T')[0];
-let stream = fs.createReadStream(file.path);
-
-// certain things are grabbed out of file, determine what were going to name it
-// promisifying crypto.randomBytes
-new Promise((resolve, reject) => {
-  // makes unique filename
-  crypto.randomBytes(16, (error, buffer) => {
-    if (error) {
-      // if cryptobytes fails we want to reject promise and log error
-      reject(error);
-    } else {
-      // making a useable filename prefix
-      resolve(buffer.toString('hex'));
-    }
-  });
-}) // returns a promise where the value is filename
-// filename gets dumped into promist chain
-// shit is getting built up
-.then((filename) => {
-  let params = {
-    ACL: 'public-read',
-    ContentType: mimeType,
-    Bucket: process.env.AWS_S3_BUCKET_NAME,
-    Key: `${folder}/${filename}${ext}`,
-    Body: stream
-  };
-
-  // create a new promise down here so we can return a promise and do more work down the promise chain
-  // by default s3 does not return a promise
-  return new Promise((resolve, reject) => {
-    s3.upload(params, function (error, data) {
-      if (error) {
-        console.log(error);
-        reject(error);
-      } else {
-        console.log(data);
-        resolve(data);
-      }
-    });
-  });
-})
-// if successful a promise is returned with the s3 upload data
-.then(console.log)
-.catch(console.error);
+s3Upload(file)
+  .then(console.log)
+  .catch(console.error);
