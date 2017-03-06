01879e47ec8e1bb30f93ef6f782aff48b9b5207b
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-03-06 12:06:19 -0500

    Add script to upload to S3
    Use s3.upload method to upload a file from my computer to my bucket on
    s3. Uses a few modules, AWS, fs, and dotenv. Next step is to tell AWS
    what type of file it's getting.

---
+++ bin/aws-upload.js
@@ -0,0 +1,29 @@
+'use strict';
+
+require('dotenv').load();
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const fs = require('fs');
+
+let file = {
+  path: process.argv[2],
+  title: process.argv[3]
+};
+
+let stream = fs.createReadStream(file.path);
+
+// console.log('stream is: ', stream);
+
+let params = {
+  ACL: 'public-read',
+  Bucket: process.env.AWS_S3_BUCKET_NAME,
+  Key: file.title,
+  Body: stream
+};
+
+// console.log(params);
+
+s3.upload(params, function (error, data) {
+  console.log(error, data);
+});
